e6d26c0d6185b4a6d8e5752fc995d2b933d8069d
Cameron <cpolly@pgh.yearup.org>
2024-09-25 13:34:04 -0400

    Fixed declaration

--- workbook_1/hello-world/src/main/java/com/pluarlsight/ShoppingList.java
+++ workbook_1/hello-world/src/main/java/com/pluarlsight/ShoppingList.java
@@ -2,8 +2,7 @@ package com.pluarlsight;
 
 public class ShoppingList {
     public static void main(String[] args) {
-        int i;
-        for (i=1; i<11; i++){
+        for (int i=1; i<11; i++){
             System.out.println("Item " + i);
         }
     }
