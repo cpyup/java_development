59b9bb8cf9c81e942d07132f79ca07883cc01587
Cameron <cpolly@pgh.yearup.org>
2024-09-25 13:25:57 -0400

    Initial Commit

---
+++ workbook_1/hello-world/src/main/java/com/pluarlsight/ShoppingList.java
@@ -0,0 +1,10 @@
+package com.pluarlsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        int i;
+        for (i=1; i<11; i++){
+            System.out.println("Item " + i);
+        }
+    }
+}
