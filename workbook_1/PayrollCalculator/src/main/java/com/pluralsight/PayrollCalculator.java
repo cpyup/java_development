e4e85b8031af0e4c4db956c59a6605d73d3abe98
Cameron <cpolly@pgh.yearup.org>
2024-09-29 21:26:50 -0400

    Completed Mod3 - Exercise 4 - Payroll Calculator with overtime pay

---
+++ workbook_1/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        GetInput();
+    }
+
+    public static void GetInput(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Name");
+        String name = input.nextLine();
+
+        System.out.println("\nEnter Hours Worked");
+        double hoursWorked = input.nextDouble();
+
+        System.out.println("\nEnter Pay Rate");
+        double payRate = input.nextDouble();
+
+        DisplayOutput(name,hoursWorked,payRate);
+
+    }
+
+    public static void DisplayOutput(String name, double hours, double rate){
+        double grossPay;
+
+        if(hours > 40){
+            grossPay = ((hours - 40) * (rate * 1.5)) + (40 * rate);
+        }else{
+            grossPay = hours*rate;
+        }
+        String gross = String.format("%.2f",grossPay);
+        System.out.println("\nEmployee: "+name+"\nGross Pay: $"+gross);
+    }
+}
