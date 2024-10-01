7a0f1d8b504d892e499de37e6f4f78585b5d3426
Cameron <cpolly@pgh.yearup.org>
2024-10-01 15:47:06 -0400

    Completed Workbook 2, Exercise 4 - TheaterReservations

---
+++ workbook_2/TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class TheaterReservations {
+    public static void main(String[] args) {
+        GetInput();
+    }
+
+    public static void GetInput(){
+        Scanner input = new Scanner(System.in);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+        System.out.println("Enter your full name: ");
+        String[] fullName = input.nextLine().trim().split(" ");
+
+        System.out.println("Enter the date (MM/dd/yyyy): ");
+        LocalDate dateIn = LocalDate.parse(input.nextLine(),format);
+
+        System.out.println("Enter desired amount of tickets: ");
+        int ticketCount = Integer.parseInt(input.nextLine());
+
+        if(ticketCount > 1){
+            System.out.println(ticketCount+" tickets reserved for "+ dateIn.format(format) + " under " + fullName[1] + ", " + fullName[0]);
+        }else{
+            System.out.println("1 ticket reserved for "+ dateIn.format(format) + " under " + fullName[1] + ", " + fullName[0]);
+        }
+    }
+}
