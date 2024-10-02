c317b6d8caa768926e9fb34cbde0a1de1f49570d
Cameron <cpolly@pgh.yearup.org>
2024-10-02 15:56:21 -0400

    Updated Solution To Condense Accessors

--- workbook_2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
+++ workbook_2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -17,51 +17,25 @@ public class CellPhone {
     }
 
     // Getter and setter for serialNumber
-    public int getSerialNumber() {
-        return serialNumber;
-    }
-
+    public int getSerialNumber() { return serialNumber; }
     public void setSerialNumber(int serialNumber) {
-        if (serialNumber >= 1000000 && serialNumber <= 9999999) {
-            this.serialNumber = serialNumber;
-        } else {
-            throw new IllegalArgumentException("Serial number must be between 1000000 and 9999999.");
-        }
+        if (serialNumber >= 1000000 && serialNumber <= 9999999) this.serialNumber = serialNumber;
+        else throw new IllegalArgumentException("Serial number must be between 1000000 and 9999999.");
     }
 
     // Getter and setter for model
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
 
     // Getter and setter for carrier
-    public String getCarrier() {
-        return carrier;
-    }
-
-    public void setCarrier(String carrier) {
-        this.carrier = carrier;
-    }
+    public String getCarrier() { return carrier; }
+    public void setCarrier(String carrier) { this.carrier = carrier; }
 
     // Getter and setter for phoneNumber
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 
     // Getter and setter for owner
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
+    public String getOwner() { return owner; }
+    public void setOwner(String owner) { this.owner = owner; }
 }
