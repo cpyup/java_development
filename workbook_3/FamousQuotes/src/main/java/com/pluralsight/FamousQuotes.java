fa4f5d39e8523d65950452c660aac6649077c4bf
Cameron <cpolly@pgh.yearup.org>
2024-10-07 17:39:44 -0400

    Populated Quote List

--- workbook_3/FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
+++ workbook_3/FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class FamousQuotes {
     public static final String[] quoteList = new String[]{
             "The only way to deal with an unfree world is to become so absolutely free that your very existence is an act of rebellion.",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10"
+            "If you are going through hell, keep going.",
+            "The smallest minority on earth is the individual. Those who deny individual rights cannot claim to be defenders of minorities.",
+            "If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment.",
+            "Death is nothing to us. When we exist, death is not; and when death exists, we are not.",
+            "The struggle itself toward the heights is enough to fill a man's heart. One must imagine Sisyphus happy.",
+            "The individual has always had to struggle to keep from being overwhelmed by the tribe. If you try it, you will be lonely often, and sometimes frightened. But no price is too high to pay for the privilege of owning yourself.",
+            "There is nothing in the desert, and no man needs nothing.",
+            "The higher we soar the smaller we appear to those who cannot fly.",
+            "I write differently from what I speak, I speak differently from what I think, I think differently from the way I ought to think, and so it all proceeds into deepest darkness."
     };
     public static Scanner scanner = new Scanner(System.in);
 
@@ -31,7 +31,7 @@ public class FamousQuotes {
     }
 
     public static Boolean handleInput() {
-        System.out.println("Enter a number (1-10) to display a quote, R for a random quote, or E to exit\n");
+        System.out.println("Enter a number (1-"+quoteList.length+") to display a quote, R for a random quote, or E to exit\n");
         String selection = scanner.nextLine().trim();
 
         switch (selection) {
