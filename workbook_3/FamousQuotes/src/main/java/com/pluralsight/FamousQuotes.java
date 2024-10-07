d260c10e5198e9921e0136b05d35fdeef13c3abd
Cameron <cpolly@pgh.yearup.org>
2024-10-07 17:17:44 -0400

    Updated Structure For Bonus Implementations

--- workbook_3/FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
+++ workbook_3/FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class FamousQuotes {
@@ -13,22 +14,49 @@ public class FamousQuotes {
             "7",
             "8",
             "9",
-            "10"};
+            "10"
+    };
     public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        while (true){
-            try{
-                handleInput();
-            }catch (Exception e){
+        while (true) {
+            try {
+                if (!handleInput()) {
+                    break;
+                }
+            } catch (Exception e) {
                 System.out.println("Invalid Input\n");
-                handleInput();
             }
         }
     }
 
-    public static void handleInput(){
-        System.out.println("Enter a number (1-10) to display a quote\n");
-        int selection = scanner.nextInt();
-        System.out.println("\""+quoteList[selection-1]+"\"\n");
+    public static Boolean handleInput() {
+        System.out.println("Enter a number (1-10) to display a quote, R for a random quote, or E to exit\n");
+        String selection = scanner.nextLine().trim();
+
+        switch (selection) {
+            case "R", "r" -> {
+                Random random = new Random();
+                int randomIndex = random.nextInt(quoteList.length);
+                System.out.println("\"" + quoteList[randomIndex] + "\"\n");
+                return true;
+            }
+            case "E", "e" -> {
+                return false;
+            }
+            default -> {
+                try {
+                    int index = Integer.parseInt(selection);
+                    if (index < 1 || index > quoteList.length) {
+                        System.out.println("Please enter a number between 1 and " + quoteList.length + ".\n");
+                    } else {
+                        System.out.println("\"" + quoteList[index - 1] + "\"\n");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter a valid number, R, or E.\n");
+                }
+                return true;
+            }
+        }
     }
 }
