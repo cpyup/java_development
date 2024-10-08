52ce48701d21f475b5fbd4e9b5c3da36cd6a2953
Cameron <cpolly@pgh.yearup.org>
2024-10-08 10:04:42 -0400

    Completed Exercise 2 - BedtimeStories

---
+++ workbook_3/Mod01/BedtimeStories/src/main/java/com/pluralsight/BedtimeStories.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BedtimeStories {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the name of the book to read\n");
+        String selection = input.nextLine().trim();
+
+        try{
+            FileInputStream fis = new FileInputStream(selection);
+            Scanner fileIn = new Scanner(fis);
+
+            String contents;
+            int lineCount = 1;
+
+            while (fileIn.hasNextLine()){
+                contents = lineCount+". "+fileIn.nextLine();
+                System.out.println(contents);
+                lineCount++;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
