be2689088958c42712c80c07e44c5787a991ad51
Cameron <cpolly@pgh.yearup.org>
2024-10-08 13:46:16 -0400

    Completed PayrollCalculator Update

--- workbook_3/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ workbook_3/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,7 +1,9 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,11 +11,24 @@ public class PayrollCalculator {
     public static ArrayList<Employee> employees = new ArrayList<>();
     public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Enter the name of the file to load");
-        String fileToLoad = input.nextLine().trim();
-
+        String fileToLoad;
+        System.out.println("Enter the name of the file to load:");
+        fileToLoad = input.nextLine().trim();
         ReadCsv(fileToLoad);
-        PrintCsv();
+
+        System.out.println("Enter the name of the file to write to:");
+        fileToLoad = input.nextLine().trim();
+
+
+        String extension = fileToLoad.split("\\.")[1];
+
+        if(extension.equalsIgnoreCase("csv")){
+            WriteCsv(fileToLoad);
+        }else if(extension.equalsIgnoreCase("json")){
+            WriteJson(fileToLoad);
+        }else{
+            System.out.println("Error With Output File Extension");
+        }
     }
 
     public static void ReadCsv(String fileName){
@@ -45,6 +60,58 @@ public class PayrollCalculator {
             }
         }catch (Exception e){
             System.out.println("Error Printing File");
+        }
+    }
+
+    public static void WriteCsv(String fileName){
+        try{
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+
+            bufferedWriter.write("id|name|gross_pay");
+            for (Employee thisEmployee : employees) {
+                String newLine = String.format("%d|%s|%.2f\n",
+                        thisEmployee.getEmployeeId(),
+                        thisEmployee.getName(),
+                        thisEmployee.getGrossPay());
+                bufferedWriter.write(newLine);
+            }
+            bufferedWriter.close();
+        }catch (Exception e){
+            System.out.println("Error Writing File");
+            e.printStackTrace();
+        }
+    }
+
+    public static void WriteJson(String fileName) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write("[\n");
+            for (int i = 0; i < employees.size(); i++) {
+                Employee thisEmployee = employees.get(i);
+
+                String jsonEmployee = String.format(  // Formatting JSON output
+                        """
+                                  {
+                                    "id": %d,
+                                    "name": "%s",
+                                    "gross_pay": %.2f
+                                  }\
+                                """,
+                        thisEmployee.getEmployeeId(),
+                        thisEmployee.getName(),
+                        thisEmployee.getGrossPay()
+                );
+                bufferedWriter.write(jsonEmployee);
+
+                if (i < employees.size() - 1) {
+                    bufferedWriter.write(",\n");
+                } else {
+                    bufferedWriter.write("\n"); // End of last object
+                }
+            }
+            bufferedWriter.write("]\n"); // End of JSON
+        } catch (Exception e) {
+            System.out.println("Error Writing JSON File");
+            e.printStackTrace();
         }
     }
 }
