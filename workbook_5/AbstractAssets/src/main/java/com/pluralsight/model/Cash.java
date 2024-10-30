e71bff5ce971f7b523e7f98f1e5164248b408b3e
Cameron <cpolly@pgh.yearup.org>
2024-10-30 09:50:21 -0400

    Completed Abstract Assets Exercise

---
+++ workbook_5/AbstractAssets/src/main/java/com/pluralsight/model/Cash.java
@@ -0,0 +1,12 @@
+package com.pluralsight.model;
+
+public class Cash extends Asset{
+    public Cash(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+
+    @Override
+    public double getValue() {
+        return getOriginalCost();
+    }
+}
