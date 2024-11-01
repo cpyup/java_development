c610704c74522e3937ca44fe38d823007cd96543
Cameron <cpolly@pgh.yearup.org>
2024-11-01 11:30:22 -0400

    Completed WorkingWithInterfaces Exercise

---
+++ workbook_6/WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+public class Person implements Comparable<Person>{
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person p) {
+        // Sort by last name, if the last name is the same, sort by first, then age
+        int last = this.lastName.compareTo(p.getLastName());
+        int first = this.firstName.compareTo(p.getFirstName());
+
+        if(last == 0 && first == 0){
+            if(this.age == p.age){
+                return 0;
+            }else if(this.age > p.age){
+                return 1;
+            }
+        }
+
+        return last == 0 ? this.firstName.compareTo(p.firstName) : last;
+    }
+}
---
+++ workbook_6/WorkingWithInterfaces/src/main/java/com/pluralsight/WorkingWithInterfaces.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class WorkingWithInterfaces {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("George","Jones", 52));
+        people.add(new Person("Oscar","Jones", 29));
+        people.add(new Person("George","Michael", 88));
+        people.add(new Person("Ezra","Aiden", 19));
+        people.add(new Person("Oscar","Jones", 32));
+
+        people.forEach(person -> System.out.println(person.getLastName()+", "+person.getFirstName()+", "+person.getAge()));
+        System.out.println("\n");
+        Collections.sort(people);
+        people.forEach(person -> System.out.println(person.getLastName()+", "+person.getFirstName()+", "+person.getAge()));
+    }
+}
