a5c0c71b6a56beecab0d0d8300579f3c85a5e8b8
Cameron <cpolly@pgh.yearup.org>
2024-11-21 09:48:12 -0500

    Modified Join Types

--- workbook_7/joins/northwind_joins.sql
+++ workbook_7/joins/northwind_joins.sql
@@ -1,14 +1,14 @@
 -- Product id, name, unit price and category of all products, order by cat, product name
 select ProductID, ProductName, UnitPrice, CategoryName
 from products
-left join categories
+inner join categories
 	on products.CategoryID = categories.CategoryID
 order by CategoryName, ProductName;
 
 -- Product id, name, price and supplier of all products over 75, order by product name
 select ProductID, ProductName, UnitPrice, suppliers.CompanyName
 from products
-left join suppliers
+inner join suppliers
 		on products.SupplierID = suppliers.SupplierID
 where UnitPrice > 75
 order by ProductName;
@@ -16,16 +16,16 @@ order by ProductName;
 -- Product id, name, price, category, and supplier, order by product name
 select ProductID, ProductName, UnitPrice, CategoryName, CompanyName
 from products
-left join categories
+inner join categories
 		on products.CategoryID = categories.CategoryID
-left join suppliers
+inner join suppliers
 		on products.SupplierID = suppliers.SupplierID
 order by products.ProductName;
 
 -- Product names & categories of most expensive products
 select ProductName, CategoryName
 from products
-left join categories
+inner join categories
 		on products.CategoryID = categories.CategoryID
 where UnitPrice = (select max(UnitPrice)
 					from products);
@@ -33,15 +33,15 @@ where UnitPrice = (select max(UnitPrice)
 -- Order id, ship name, ship address, shipping company from every order to Germany
 select OrderID, ShipName, ShipAddress, shippers.CompanyName
 from orders
-left join shippers
+inner join shippers
 		on orders.ShipVia = shippers.ShipperID
-where ShipCountry = "Germany";
+where ShipCountry = 'Germany';
 
 -- Order id, date, ship name, address of all orders including Sasquatch Ale
 select orders.OrderID, orders.OrderDate, orders.ShipName, orders.ShipAddress
 from orders
-left join `order details`
+inner join `order details`
 		on orders.OrderID = `order details`.OrderID
-left join products
+inner join products
 		on products.ProductID = `order details`.ProductID
 where ProductName = 'Sasquatch Ale';
