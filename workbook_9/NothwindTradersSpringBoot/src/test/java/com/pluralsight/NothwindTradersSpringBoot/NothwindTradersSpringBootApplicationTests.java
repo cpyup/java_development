cfb5ec601ff5684f4bfa54238831693aaae73786
Cameron <cpolly@pgh.yearup.org>
2024-12-05 11:34:26 -0500

    Completed NorthwindTraders Spring Boot Exercise

---
+++ workbook_9/NothwindTradersSpringBoot/src/test/java/com/pluralsight/NothwindTradersSpringBoot/NothwindTradersSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NothwindTradersSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NothwindTradersSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
